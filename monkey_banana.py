f822ab6e01446fc11033a9156cab89f0088d4ef0
saiganesh08 <93111749+saiganesh08@users.noreply.github.com>
2022-11-30 21:39:27 +0530

    Add files via upload

---
+++ monkey_banana.py
@@ -0,0 +1,22 @@
+import random
+ 
+class Monkey:
+	def __init__(self, bananas):
+		self.bananas = bananas
+ 
+	def __repr__(self):
+		return "Monkey with %d bananas." % self.bananas
+ 
+monkeys = [Monkey(random.randint(0, 50)) for i in range(5)]
+ 
+ 
+print ("Random monkeys:",monkeys)
+ 
+def number_of_bananas(monkey):
+    """Returns number of bananas that monkey has."""
+    return monkey.bananas
+ 
+print("number_of_bananas( FIRST MONKEY ): ", number_of_bananas(monkeys[0]))
+ 
+max_monkey = max(monkeys, key=number_of_bananas)
+print( "Max monkey: ", max_monkey)
